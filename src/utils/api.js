e9779873a02ad1a600afe83c62e63ee3422e0026
mo3gza <amiracle4001@gmail.com>
2025-08-17 17:36:43 +0300

    Fix Api Base url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { API_CONFIG, getApiUrl } from '../config/api';
+import { API_CONFIG } from '../config/api';
 
 // Create axios instance with mobile-friendly configuration
 const api = axios.create({
